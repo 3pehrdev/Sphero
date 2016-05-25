02797b436aca8024e70e263ee882dc071ec30cb3
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-25 22:15:16 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,6 +1,6 @@
 do function run(msg, matches)
 if matches[1]:lower() == 'id' and is_momod(msg) then
-local text = "["..msg.from.print_name.."](https://telegram.me/"..msg.from.username..")\n🔺*Your ID* : _"..msg.from.id.."_\n🔻*Group ID* : _"..msg.to.id.."_"
+local text = "["..msg.from.print_name.."](https://telegram.me/"..msg.from.username..")\n*Your ID* : _"..msg.from.id.."_\n*Group ID* : _"..msg.to.id.."_"
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 end
