0742f028ac5d57c4a464a21f986897acfb69fcf2
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-31 12:58:58 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -3,10 +3,18 @@ if matches[1]:lower() == 'id' and is_momod(msg) then
 local text = "["..msg.from.print_name.."](https://telegram.me/"..msg.from.username..")\n*Your ID* : _"..msg.from.id.."_\n*Group ID* : _"..msg.to.id.."_"
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
+local function(msg ,matches)
+if matches[2]:lower() == 'id' And not is_momod(msg) then 
+local text = `sik kooniiiiiii`\n *Only For moderators*
+send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+end
 end
 return {
 description = "show your id",
-usage = "!id : show your userid and information",
+usage = {
+moderator = {
+"!id : show your userid and information" },
+},
 patterns = {
 "^[!/#]([Ii][Dd])$",
 "^[Ii][Dd]$",
