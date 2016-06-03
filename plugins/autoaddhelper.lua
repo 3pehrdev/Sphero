4fd2ef8760e2d379911a425910ea67eef554de95
3pehrdev <sepehrsalehi0111@gmail.com>
2016-06-04 02:50:21 +0430

    Update autoaddhelper.lua

--- plugins/autoaddhelper.lua
+++ plugins/autoaddhelper.lua
@@ -7,7 +7,6 @@ local function run(msg, matches)
 local sudo = 230857953 --Put you id Here !
 local addsudo = 'user#id'..sudo
 local chat = get_receiver(msg)
-if is_momod(msg) then -- you can set it to is_owner(msg)
 chat_add_user(chat, addsudo, callback, false)
 end
 end
