4352aa3fc299aabca77472c91d503fe8115fb24c
3pehrdev <sepehrsalehi0111@gmail.com>
2016-06-13 00:14:32 +0430

    Update setlink.lua

--- plugins/setlink.lua
+++ plugins/setlink.lua
@@ -2,20 +2,34 @@
 		if matches[1]:lower() == 'setlink' and is_owner(msg) then
 			data[tostring(msg.to.id)]['settings']['set_link'] = 'waiting'
 			save_data(_config.moderation.data, data)
-			return 'Please Send Me Your New Group Link!'
+			return 'لطفا لینک جدید برای گروه رو ارسال کنید.'
 		end
 
 		if msg.text then
 			if msg.text:match("^(https://telegram.me/joinchat/%S+)$") and data[tostring(msg.to.id)]['settings']['set_link'] == 'waiting' and is_owner(msg) then
 				data[tostring(msg.to.id)]['settings']['set_link'] = msg.text
 				save_data(_config.moderation.data, data)
-				return "Link Has Been Saved!"
+				return "لینک جدید ثبت شد."
 			end
-	end
+		end
+		if matches[1]:lower() == 'ثبت لینک' and is_owner(msg) then
+			data[tostring(msg.to.id)]['settings']['set_link'] = 'waiting'
+			save_data(_config.moderation.data, data)
+			return 'لطفا لینک جدید برای گروه رو ارسال کنید.'
+		end
+
+		if msg.text then
+			if msg.text:match("^(https://telegram.me/joinchat/%S+)$") and data[tostring(msg.to.id)]['settings']['set_link'] == 'waiting' and is_owner(msg) then
+				data[tostring(msg.to.id)]['settings']['set_link'] = msg.text
+				save_data(_config.moderation.data, data)
+				return "لینک جدید ثبت شد."
+  end
+			end
 end
 return {
   patterns = {
  "^[!#/](setlink)$",
+ -- "^(ثبت لینک)$",
     "^(https://telegram.me/joinchat/%S+)$",
   },
   run = run
