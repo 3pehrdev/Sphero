5bde28149c9c360c2b8a1f8c3c3f19dc9bc5c6e6
3pehrdev <sepehrsalehi0111@gmail.com>
2016-06-13 00:15:45 +0430

    Update setlink.lua

--- plugins/setlink.lua
+++ plugins/setlink.lua
@@ -2,14 +2,14 @@
 		if matches[1]:lower() == 'setlink' and is_owner(msg) then
 			data[tostring(msg.to.id)]['settings']['set_link'] = 'waiting'
 			save_data(_config.moderation.data, data)
-			return 'لطفا لینک جدید برای گروه رو ارسال کنید.'
+			return 'Please Send Me New Group Link!'
 		end
 
 		if msg.text then
 			if msg.text:match("^(https://telegram.me/joinchat/%S+)$") and data[tostring(msg.to.id)]['settings']['set_link'] == 'waiting' and is_owner(msg) then
 				data[tostring(msg.to.id)]['settings']['set_link'] = msg.text
 				save_data(_config.moderation.data, data)
-				return "لینک جدید ثبت شد."
+				return "Link Has Been Saved!"
 			end
 		end
 		if matches[1]:lower() == 'ثبت لینک' and is_owner(msg) then
